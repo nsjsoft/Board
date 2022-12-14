8ba874e4531ed77ff0f62f2d2f3edb1c75b9fae3
nsjsoft <nsj0409@gmail.com>
2022-12-15 02:46:20 +0900

    common.dto

---
+++ src/main/java/com/board/common/dto/MessageDto.java
@@ -0,0 +1,19 @@
+package com.board.common.dto;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageDto {
+
+	private String message;
+	private String redirectUri;
+	private RequestMethod method;
+	private Map<String, Object> data;
+
+}
